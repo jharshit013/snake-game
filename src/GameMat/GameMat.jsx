7b2b58df0ade975000e36a9a9b838b44a91056a3
jharshit013 <jharshit013@gmail.com>
2021-07-30 20:03:58 +0530

    snake growth implemented

--- src/GameMat/GameMat.jsx
+++ src/GameMat/GameMat.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {randomNumGenerator,reverseLinkedList,useInterval} from '../utils/utils.js';
+import {randomNumGenerator,useInterval} from '../utils/utils.js';
 
 import './GameMat.css'
 
@@ -18,13 +18,13 @@ class LinkedList{
     }
 }
 
-class Cell{
-    constructor(row,col,val){
-        this.row=row;
-        this.col=col;
-        this.val=val;
-    }
-}
+// class Cell{
+//     constructor(row,col,val){
+//         this.row=row;
+//         this.col=col;
+//         this.val=val;
+//     }
+// }
 
 const Movements={
     UP:'UP',
@@ -59,16 +59,21 @@ const GameMat=()=>{
 
 
     useEffect(()=>{
-        setInterval(()=>{
-            // moveSnake();
-        }, 1000);
-
         window.addEventListener('keydown',e=>{
-            const newDirection=getDirectionFromKey(e.key);
-            const isValidDirection=newDirection!=='';
-            if(isValidDirection) setDirection(newDirection);
-        })
+            handleKeyDown(e);
+        });
     },[]);
+
+    useInterval(()=>{
+        moveSnake();
+    },100);
+
+    const handleKeyDown=e=>{
+        const newDirectn=getDirectionFromKey(e.key);
+            const isValidDirection=newDirectn!=='';
+            if(!isValidDirection) return;
+            setDirection(newDirectn);
+    }
 
     const moveSnake=()=>{
         // const currSnakeHead=snake.head;
@@ -90,59 +95,55 @@ const GameMat=()=>{
             handleGameOver();
             return;
         }
-
-        const isFoodEaten= nextSnakeHeadValue===foodCell;
-        if(isFoodEaten){
-            handleFoodEaten();
-            // handleGrowth(nextSnakeHeadCords);
-        } 
-
+ 
         const newSnakeHead=new Node({
             row: nextSnakeHeadCords.row,
             col: nextSnakeHeadCords.col,
             cell: nextSnakeHeadValue,
         });
+
+        const currSnakeHead=snake.head;
+        snake.head=newSnakeHead;
+        currSnakeHead.next=newSnakeHead;
         
         const newSnakeCells=new Set(snakeCells);
         newSnakeCells.delete(snake.tail.val.cell);
         newSnakeCells.add(nextSnakeHeadValue)
         
-        snake.head=newSnakeHead;
+        snake.tail=snake.tail.next;
+        if(snake.tail===null) snake.tail=snake.head;
 
-        if(!isFoodEaten){
-            snake.tail=snake.tail.next;
-            if(snake.tail===null) snake.tail=snake.head;
+        const isFoodEaten= nextSnakeHeadValue===foodCell;
+        if(isFoodEaten){
+            handleGrowth(newSnakeCells);
+            handleFoodEaten();
         }
+
+        // if(!isFoodEaten){
+        //     snake.tail=snake.tail.next;
+        //     if(snake.tail===null) snake.tail=snake.head;
+        // }
 
         setSnakeCells(newSnakeCells)
     };
 
-    const getNextSnakeHeadCords=(currSnakeHeadCords,direction)=>{
-        if(direction===Movements.UP){
-            return {
-                row: currSnakeHeadCords.row-1,
-                col: currSnakeHeadCords.col,
-            };
+    const handleGrowth=newSnakeCells=>{
+        const growthNodeCoords=getGrowthNodeCoords(snake.tail,direction);
+        if(isOutOfMat(growthNodeCoords,mat)){
+            return;
         }
-        if(direction===Movements.LEFT){
-            return {
-                row: currSnakeHeadCords.row,
-                col: currSnakeHeadCords.col-1,
-            };
-        }
-        if(direction===Movements.DOWN){
-            return {
-                row: currSnakeHeadCords.row+1,
-                col: currSnakeHeadCords.col,
-            };
-        }
-        if(direction===Movements.RIGHT){
-            return {
-                row: currSnakeHeadCords.row,
-                col: currSnakeHeadCords.col+1,
-            };
-        }
-    };
+        const newSnakeTailVal=mat[growthNodeCoords.row][growthNodeCoords.col];
+        const newSnakeTail=new Node({
+            row: growthNodeCoords.row,
+            col: growthNodeCoords.col,
+            cell: newSnakeTailVal,
+        });
+        const currTail=snake.tail;
+        snake.tail=newSnakeTail;
+        snake.tail.next=currTail;
+
+        newSnakeCells.add(newSnakeTailVal);
+    }
 
     const handleFoodEaten=()=>{
         const totalCells=GAMEMAT_SIZE*GAMEMAT_SIZE;
@@ -154,15 +155,6 @@ const GameMat=()=>{
         }
         setFoodCell(nextFoodCell);
         setScore(score+1);
-    }
-
-    const handleGrowth=(nextSnakeHeadCords)=>{
-        const nextSnakeHeadValue=mat[nextSnakeHeadCords.row][nextSnakeHeadCords.col];
-        const newSnakeHead=new Node({
-            row: nextSnakeHeadCords.row,
-            col: nextSnakeHeadCords.col,
-            cell: nextSnakeHeadValue,
-        });
     }
 
     const handleGameOver=()=>{
@@ -177,12 +169,11 @@ const GameMat=()=>{
     return (
         <>
         <h1>Score:{score}</h1>
-        <button onClick={()=>moveSnake()}>Manual</button>
         <div className="mat">
             {mat.map((row,rowIndex)=>(
                 <div kew={rowIndex} className="row">{
                     row.map((cellValue,cellIndex)=>(
-                        <div key={cellIndex} className={`cell ${snakeCells.has(cellValue) ? 'snake-cell': ''} ${foodCell===cellValue?'food-cell':''}`}>{cellValue}</div>
+                        <div key={cellIndex} className={`cell ${snakeCells.has(cellValue) ? 'snake-cell': ''} ${foodCell===cellValue?'food-cell':''}`}></div>
                     ))
                 }</div>
             ))}
@@ -206,6 +197,33 @@ const makeMat=GAMEMAT_SIZE=>{
 
 };
 
+const getNextSnakeHeadCords=(currSnakeHeadCords,direction)=>{
+    if(direction===Movements.UP){
+        return {
+            row: currSnakeHeadCords.row-1,
+            col: currSnakeHeadCords.col,
+        };
+    }
+    if(direction===Movements.LEFT){
+        return {
+            row: currSnakeHeadCords.row,
+            col: currSnakeHeadCords.col-1,
+        };
+    }
+    if(direction===Movements.DOWN){
+        return {
+            row: currSnakeHeadCords.row+1,
+            col: currSnakeHeadCords.col,
+        };
+    }
+    if(direction===Movements.RIGHT){
+        return {
+            row: currSnakeHeadCords.row,
+            col: currSnakeHeadCords.col+1,
+        };
+    }
+};
+
 const getDirectionFromKey=key=>{
     if(key==='ArrowUp') return Movements.UP;
     if(key==='ArrowLeft') return Movements.LEFT;
@@ -219,7 +237,44 @@ const isOutOfMat=(coords,mat)=>{
     const {row,col}=coords;
     if(row<0 || col<0 || row>=mat.length || col>=mat.length) return true;
     return false;
-}
+};
+
+const getGrowthNodeCoords=(snakeTail,currtDirectn)=>{
+    const tailNextNodeDirectn=getNextNodeDirection(snakeTail,currtDirectn);
+    const growthDirectn=getOppositeDirection(tailNextNodeDirectn);
+    const currTailCoords={
+        row: snakeTail.val.row,
+        col: snakeTail.val.col,
+    };
+    const growthNodeCoords=getNextSnakeHeadCords(currTailCoords,growthDirectn);
+    return growthNodeCoords;
+};
+
+const getNextNodeDirection=(node,currDirectn)=>{
+    if(node.next===null) return currDirectn;
+    const {row: currRow, col: currCol}=node.val;
+    const {row: nextRow, col: nextCol}=node.next.val;
+    if(nextRow===currRow-1 && nextCol===currCol){
+        return Movements.UP;
+    }
+    if(nextRow===currRow && nextCol===currCol-1){
+        return Movements.LEFT;
+    }
+    if(nextRow===currRow+1 && nextCol===currCol){
+        return Movements.DOWN;
+    }
+    if(nextRow===currRow && nextCol===currCol+1){
+        return Movements.RIGHT;
+    }
+    return '';
+};
+
+const getOppositeDirection=direction=>{
+    if(direction===Movements.UP) return Movements.DOWN;
+    if(direction===Movements.LEFT) return Movements.RIGHT;
+    if(direction===Movements.DOWN) return Movements.UP;
+    if(direction===Movements.RIGHT) return Movements.LEFT;
+};
 
 export default GameMat;
 
